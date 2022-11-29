00be378e2377d2c55aabb4e285ed1370e3f96355
AlexeyYuzh19 <9676331836@mail.ru>
2022-11-29 12:04:18 +0500

    написана программа 5

---
+++ Example005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Приветствую Вас, введите Ваше имя :");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+      Console.WriteLine("Ооо... это же сама МАРИЯ ВЕЛИКАЯ! Низкий Вам поклон и глубокая признательность за уделенное внимание!");
+}
+else
+{
+      Console.Write("Привет, "); 
+      Console.WriteLine(username);
+}
+
+
